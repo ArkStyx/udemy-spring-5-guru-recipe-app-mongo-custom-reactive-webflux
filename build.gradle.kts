bb8562fdb9babe53c47d95f960944c4f46a94d83
ArkStyx <regis.petit@gmail.com>
2023-05-12 23:24:52 +0200

    commit rapide

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-validation:2.7.6")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.webjars:bootstrap:3.3.7-1")
 	implementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
 
--- src/main/java/guru/springframework/recipe/app/exceptions/NotFoundException.java
+++ src/main/java/guru/springframework/recipe/app/exceptions/NotFoundException.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
 
-	// TODO SERIAL VERSION ID ?????
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
     public NotFoundException() {
         super();
